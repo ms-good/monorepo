372b70477a4487aed1d6d175864a7b3d231bd483
yoonminsnag <apdlyooapdl@naver.com>
2023-08-30 02:40:10 +0900

    feat: design 레포와 timer 레포 생성 및 세팅
    - design 레포에서는 디자인 관련된 공통 코드를 작성합니다. 현재 스토리북만 설치한 상태이고 공통 컴포넌트를 추가하면서 테스트코드를 추가할 예정입니다.
    - timer 레포에서는 design 레포를 import해서 정상적으로 받아온 상태이고 그 외외에는 아직 아무것도 작업하지 않았습니다.

---
+++ packages/timer/client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button, Input } from '@client/design';
+
+function App() {
+  return (
+    <>
+      <Button>button</Button>
+      <Input value="input" />
+    </>
+  );
+}
+
+export default App;
