d49c499350f047c8e416d8e71beddbb19a1033fd
yoonminsnag <apdlyooapdl@naver.com>
2023-08-31 01:48:14 +0900

    feat: Timer 컴포넌트 기본 뼈대 작성, 탭 기능 및 ui 구현

--- packages/timer/client/src/App.tsx
+++ packages/timer/client/src/App.tsx
@@ -1,12 +1,46 @@
-import { Button, Input } from '@client/design';
+import { useState } from 'react';
+import tw from 'twin.macro';
+// import { Button, Input } from '@client/design';
+
+// TODO: 디자인 시스템을 만들고 디자인 시스템 컴포넌트를 이용하게 수정
+
+type TabState = 'timer' | 'stopwatch';
+const tabs: TabState[] = ['timer', 'stopwatch'];
 
 function App() {
+  const [currentTab, setCurrentTab] = useState<TabState>('timer');
+
   return (
-    <>
-      <Button>button</Button>
-      <Input value="input" />
-    </>
+    <div css={tw`max-w-3xl mx-auto`}>
+      <h1 css={tw`flex justify-center p-10 text-5xl font-bold`}>TIMER</h1>
+      <div css={tw`flex flex-col`}>
+        <ul css={tw`flex w-full`}>
+          {tabs.map((tab) => {
+            const isSelected = currentTab === tab;
+            return (
+              <li
+                css={[
+                  tw`w-1/2 text-center text-xl cursor-pointer text-gray-500 border-b-0 border-gray-500`,
+                  isSelected && tw`text-blue-500 border-b-2 border-blue-500`,
+                ]}
+                onClick={() => setCurrentTab(tab)}>
+                {tab}
+              </li>
+            );
+          })}
+        </ul>
+        {currentTab === 'timer' ? <Timer /> : <StopWatch />}
+      </div>
+    </div>
   );
 }
 
 export default App;
+
+function Timer() {
+  return <div css={tw`w-full h-96 bg-neutral-200`}>Timer</div>;
+}
+
+function StopWatch() {
+  return <div css={tw`w-full h-96 bg-neutral-200`}>StopWatch</div>;
+}
