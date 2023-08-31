beb99c9e92d6b923f69de1b198f90d8bbebc799c
yoonminsnag <apdlyooapdl@naver.com>
2023-08-31 23:06:52 +0900

    refactor: 컴포넌트를 외부 파일로 이동

---
+++ packages/timer/client/src/components/StopWatch/StopWatch.tsx
@@ -0,0 +1,5 @@
+import tw from 'twin.macro';
+
+export function StopWatch() {
+  return <div css={tw`w-full h-96 bg-neutral-200`}>StopWatch</div>;
+}
---
+++ packages/timer/client/src/components/Timer/Timer.tsx
@@ -0,0 +1,5 @@
+import tw from 'twin.macro';
+
+export function Timer() {
+  return <div css={tw`w-full h-96 bg-neutral-200`}>Timer</div>;
+}
