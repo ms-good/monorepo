01aae8ac4028f59e2eb688e38aa4e5d469f6b94a
yoonminsnag <apdlyooapdl@naver.com>
2023-09-12 22:33:04 +0900

    feat: StopWatch 페이지 구현

--- packages/clock/client/src/components/StopWatch/StopWatch.tsx
+++ packages/clock/client/src/components/StopWatch/StopWatch.tsx
@@ -1,5 +1,82 @@
-import tw from 'twin.macro';
+import React from 'react';
+import tw, { css } from 'twin.macro';
+import { msToTimeString } from '@/utils';
+import { Button } from '../Button';
+import { useStopWatch } from './useStopWatch';
 
 export function StopWatch() {
-  return <div css={tw`h-full bg-neutral-200`}>StopWatch</div>;
+  const { state, recordData, currentTime, handleSectionRecord, handleStart, handleContinue, handlePause, handleInit } =
+    useStopWatch();
+
+  const buttons = (() => {
+    switch (state) {
+      case 'init':
+        return (
+          <>
+            <Button disabled>Record</Button>
+            <Button onClick={handleStart}>Start</Button>
+          </>
+        );
+      case 'progress':
+        return (
+          <>
+            <Button onClick={handleSectionRecord}>Record</Button>
+            <Button onClick={handlePause}>Pause</Button>
+          </>
+        );
+      case 'pause':
+        return (
+          <>
+            <Button onClick={handleInit}>Init</Button>
+            <Button onClick={handleContinue}>Continue</Button>
+          </>
+        );
+    }
+  })();
+
+  return (
+    <div css={tw`h-full bg-neutral-200`}>
+      <div tw="w-80 mx-auto flex flex-col gap-2">
+        <div>{msToTimeString(currentTime)}</div>
+        <div
+          css={css`
+            ${tw`flex gap-2`}
+            > * {
+              ${tw`w-20`}
+            }
+          `}>
+          {buttons}
+        </div>
+        <div>
+          {recordData.length ? (
+            <div
+              css={css`
+                ${tw`grid gap-2`}
+              `}>
+              <div
+                css={css`
+                  ${RecordDataStyle}
+                  ${tw`border-b-2 border-gray-500`}
+                `}>
+                <div>Index</div>
+                <div>Record</div>
+                <div>Full Time</div>
+              </div>
+              <div tw="overflow-auto h-56">
+                {recordData.reverse().map(({ fullTime, index, sectionRecord }) => (
+                  <div css={RecordDataStyle}>
+                    <div>{index}</div>
+                    <div>{msToTimeString(sectionRecord)}</div>
+                    <div>{msToTimeString(fullTime)}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
 }
+
+const RecordDataStyle = tw`grid [grid-template-columns: 1fr 2fr 2fr] text-center`;
