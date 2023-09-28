375c2c3d71ea84c6891a1f98429152e8117e27e7
yoonminsnag <apdlyooapdl@naver.com>
2023-09-28 18:24:04 +0900

    settig: 중복된 extends 설정 제거

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,13 +15,7 @@ module.exports = {
     ecmaFeatures: { jsx: true },
   },
 
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'prettier'],
   plugins: ['@typescript-eslint', 'import', 'react', 'react-hooks', '@emotion'],
   settings: { 'import/resolver': { typescript: {} }, react: { version: 'detect' } },
   rules: {
